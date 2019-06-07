d2b2579452d2069b3480285431f6a950fe1095d1
Amir Hesham <amirhesham65@gmail.com>
2019-06-07 22:32:38 +0200

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Amir Hesham on 6/7/19.
+//  Copyright © 2019 Amir Hesham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
